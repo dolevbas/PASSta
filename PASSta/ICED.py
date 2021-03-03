82b8fe13d67a9bec03a479ad8828f665894bd262
dolevbas <53379662+dolevbas@users.noreply.github.com>
2021-03-03 12:17:21 +0200

    Add files via upload

---
+++ PASSta/ICED.py
@@ -0,0 +1,36 @@
+from scipy.spatial import distance
+import numpy as np
+from PASSta import WED
+
+def ICED(X, Y):
+    """
+    Testing for Equal Distributions of two planetary systems samples using WED metric.
+
+    Parameters
+    ----------
+    ~X : list of 'N' planetary systems X_i (see WED format) in following format: [X_1, X_2,...,X_N]
+    ~Y : list of 'M' planetary systems Y_i (see WED format) in following format: [Y_1, Y_2,...,Y_N]
+
+    Returns
+    -------
+     A two-sample Energy Distance
+
+    Examples
+    --------
+    """
+
+    N, M = len(X), len(Y)
+
+    # Check if X or Y are empty
+    if N == 0 or M == 0:
+        raise ValueError("ICED assumes both X and Y are not empty")
+
+    A = sum(sum(np.array([[WED(i, j) for j in Y] for i in X]))) / (N * M)
+
+    B = sum(sum(np.array([[WED(i, j) for j in X] for i in X]))) / (N * N)
+
+    C = sum(sum(np.array([[WED(i, j) for j in Y] for i in Y]))) / (M * M)
+
+    e = 2 * A - B - C
+
+    return e**0.5
